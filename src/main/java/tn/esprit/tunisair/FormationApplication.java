7776947d6567a3fc465318630b561873438b6e8b
nihel25 <nihel.riahi@esprit.tn>
2023-11-01 17:31:02 +0100

    first commit

--- src/main/java/tn/esprit/tunisair/FormationApplication.java
+++ src/main/java/tn/esprit/tunisair/FormationApplication.java
@@ -2,11 +2,6 @@ package tn.esprit.tunisair;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.util.unit.DataSize;
-
-import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 
@@ -19,17 +14,10 @@ public class FormationApplication  {
 	public static void main(String[] args) {
 		SpringApplication.run(FormationApplication.class, args);
 
-		String input="howtokill";
-		String output = tn.esprit.tunisair.BadWordFilter.getCensoredText(input);
+
 
 	}
 
 
-	@Bean
-	public MultipartConfigElement multipartConfigElement() {
-		MultipartConfigFactory factory = new MultipartConfigFactory();
-		factory.setMaxFileSize(DataSize.ofMegabytes(10));
-		factory.setMaxRequestSize(DataSize.ofMegabytes(200));
-		return factory.createMultipartConfig();
-	}
+
 }

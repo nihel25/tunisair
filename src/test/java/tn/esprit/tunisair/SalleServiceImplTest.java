bfe34721cb1907ff8cd55c76cc3aceb33f70ad92
nihel25 <nihel.riahi@esprit.tn>
2023-11-11 10:25:32 +0100

    first commit

--- src/test/java/tn/esprit/tunisair/SalleServiceImplTest.java
+++ src/test/java/tn/esprit/tunisair/SalleServiceImplTest.java
@@ -25,35 +25,39 @@ import static org.mockito.Mockito.*;
     @InjectMocks
     private SalleServiceImpl salleService;
 
+    @Test
+    @Order(1)
+    void testSaveSalle() {
+        // Créer un exemple de SalleDTO
+        SalleDTO salleDTO = new SalleDTO(100L, "jerba1011", 200L);
+        Salle salle = SalleDTO.toentity(salleDTO);
+
+        // Définir le comportement du mock repository
+        when(salleRepository.save(any(Salle.class))).thenReturn(salle);
+
+        // Appeler la méthode et vérifier les résultats
+        SalleDTO savedSalle = salleService.save(salleDTO);
+
+        // Comparer les propriétés individuelles
+        assertEquals(salleDTO.getId(), savedSalle.getId());
+        assertEquals(salleDTO.getNomsalle(), savedSalle.getNomsalle());
+        assertEquals(salleDTO.getNombreplace(), savedSalle.getNombreplace());
+
+        // Vérifier que la méthode save de SalleRepository a été appelée une fois avec n'importe quel objet Salle
+        verify(salleRepository, times(1)).save(any(Salle.class));
+    }
+}
 //    @Test
 //    @Order(1)
 //    void testSaveSalle() {
 
-//        SalleDTO salleDTO = new SalleDTO(3L,"jerba1",200L);
+//        SalleDTO salleDTO = new SalleDTO(50L, "jerba1", 200L);
 //        Salle salle = SalleDTO.toentity(salleDTO);
-//
-
 //        when(salleRepository.save(salle)).thenReturn(salle);
-//
-
-//        SalleDTO savedSalle = salleService.save(salleDTO);
-//        assertEquals(salle, savedSalle);
-//
-
-    //        verify(salleRepository, times(1)).save(salle);
+//        SalleDTO savedSalleDTO = salleService.save(salleDTO);
+//        assertEquals(salleDTO.getId(), savedSalleDTO.getId());
+//        assertEquals(salleDTO.getNomsalle(), savedSalleDTO.getNomsalle());
+//        assertEquals(salleDTO.getNombreplace(), savedSalleDTO.getNombreplace());
+//        verify(salleRepository, times(1)).save(any(Salle.class));
 //    }
 //}
-    @Test
-    @Order(1)
-    void testSaveSalle() {
-        // Créez un exemple de SalleDTO
-        SalleDTO salleDTO = new SalleDTO(50L, "jerba1", 200L);
-        Salle salle = SalleDTO.toentity(salleDTO);
-        when(salleRepository.save(salle)).thenReturn(salle);
-        SalleDTO savedSalleDTO = salleService.save(salleDTO);
-        assertEquals(salleDTO.getId(), savedSalleDTO.getId());
-        assertEquals(salleDTO.getNomsalle(), savedSalleDTO.getNomsalle());
-        assertEquals(salleDTO.getNombreplace(), savedSalleDTO.getNombreplace());
-        verify(salleRepository, times(1)).save(any(Salle.class));
-    }
-}

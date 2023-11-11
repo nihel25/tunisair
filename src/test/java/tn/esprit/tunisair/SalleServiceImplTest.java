46e3e5a8587a143db477884534814228170b4061
nihel25 <nihel.riahi@esprit.tn>
2023-11-11 10:41:47 +0100

    first commit

--- src/test/java/tn/esprit/tunisair/SalleServiceImplTest.java
+++ src/test/java/tn/esprit/tunisair/SalleServiceImplTest.java
@@ -1,5 +1,6 @@
 package tn.esprit.tunisair;
 
+
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -12,12 +13,16 @@ import tn.esprit.tunisair.entity.Salle;
 import tn.esprit.tunisair.repository.SalleRepository;
 import tn.esprit.tunisair.service.SalleServiceImpl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
- class SalleServiceImplTest {
+public class SalleServiceImplTest {
 
     @Mock
     private SalleRepository salleRepository;
@@ -28,33 +33,71 @@ import static org.mockito.Mockito.*;
     @Test
     @Order(1)
     void testSaveSalle() {
-//
-        SalleDTO salleDTO = new SalleDTO();
+        // Créer des données de test
+        SalleDTO salleDTO = new SalleDTO(); // Initialisez avec des valeurs appropriées
         Salle salle = SalleDTO.toentity(salleDTO);
 
-//
+        // Définir le comportement simulé du repository
         when(salleRepository.save(salle)).thenReturn(salle);
 
-//
-        SalleDTO savedSalle = salleService.save(salleDTO);
-        assertEquals(salle, savedSalle);
+        // Appeler la méthode du service
+        SalleDTO savedSalleDTO = salleService.save(salleDTO);
 
-//
-            verify(salleRepository, times(1)).save(salle);
-   }
-   }
-//}
-//    @Test
-//    @Order(1)
-//    void testSaveSalle() {
+        // Vérifier les résultats
+        assertEquals(salleDTO, savedSalleDTO);
+        // Ajoutez d'autres vérifications en fonction de votre logique métier et de la conversion DTO.
+    }
 
-//        SalleDTO salleDTO = new SalleDTO(50L, "jerba1", 200L);
-//        Salle salle = SalleDTO.toentity(salleDTO);
-//        when(salleRepository.save(salle)).thenReturn(salle);
-//        SalleDTO savedSalleDTO = salleService.save(salleDTO);
-//        assertEquals(salleDTO.getId(), savedSalleDTO.getId());
-//        assertEquals(salleDTO.getNomsalle(), savedSalleDTO.getNomsalle());
-//        assertEquals(salleDTO.getNombreplace(), savedSalleDTO.getNombreplace());
-//        verify(salleRepository, times(1)).save(any(Salle.class));
-//    }
-//}
+    @Test
+    @Order(2)
+    void testRecherch() {
+        // Créer des données de test
+        Long salleId = 11L;
+        Salle salle = new Salle();
+        salle.setId(salleId);
+        SalleDTO expectedSalleDTO = SalleDTO.fromEntity(salle);
+
+        // Définir le comportement simulé du repository
+        when(salleRepository.findById(salleId)).thenReturn(Optional.of(salle));
+
+        // Appeler la méthode du service
+        SalleDTO foundSalleDTO = salleService.recherch(salleId);
+
+        // Vérifier les résultats
+        assertEquals(expectedSalleDTO, foundSalleDTO);
+    }
+
+    @Test
+    @Order(3)
+    void testDelete() {
+        // Créer des données de test
+        Long salleId = 1L;
+
+        // Appeler la méthode du service
+        salleService.delete(salleId);
+
+        // Vérifier que la méthode deleteById du repository a été appelée une fois avec le bon argument
+        verify(salleRepository, times(1)).deleteById(salleId);
+    }
+
+    @Test
+    @Order(4)
+    void testFindAllSalle() {
+        // Créer des données de test
+        Salle salle1 = new Salle();
+        Salle salle2 = new Salle();
+        List<Salle> salleList = new ArrayList<>();
+        salleList.add(salle1);
+        salleList.add(salle2);
+
+        // Définir le comportement simulé du repository
+        when(salleRepository.findAll()).thenReturn(salleList);
+
+        // Appeler la méthode du service
+        List<SalleDTO> foundSalleDTOs = salleService.findAllSalle();
+
+        // Vérifier les résultats
+        assertEquals(salleList.size(), foundSalleDTOs.size());
+        // Ajoutez d'autres vérifications en fonction de votre logique métier et de la conversion DTO.
+    }
+}
